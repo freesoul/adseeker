dbbc97b752ca87d6049f3322add985f8ce76fdeb
Fran Kener <kener18@gmail.com>
2018-07-13 19:59:15 +0200

    In process of adding scheduled execution and sending e-mails

--- main.py
+++ main.py
@@ -1,5 +1,113 @@
 # -*- encoding: utf-8 -*-
+
+import logging
+import json
+
+import smtplib
+
 from modules.milanuncios import Milanuncios
+
+
+class Adseeker():
+
+
+    def __init__(self):
+
+        self._set_logger();
+        self.logger.info("Starting")
+
+        self._load_config()
+        self.logger.info("Loaded configuration")
+
+
+
+    def _set_logger(self):
+
+        logFormatter = logging.Formatter("%(asctime)s:  %(message)s")
+        self.logger = logging.getLogger('adseeker')
+
+        fileHandler = logging.FileHandler("adseeker.log")
+        fileHandler.setFormatter(logFormatter)
+        self.logger.addHandler(fileHandler)
+
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setFormatter(logFormatter)
+        self.logger.addHandler(consoleHandler)
+
+        self.logger.setLevel(logging.INFO)
+
+
+
+
+    def _load_config(self):
+
+        try:
+            with open("config_emails.json", "r") as f:
+                data = f.read()
+            emails = json.loads(data)
+
+            with open("config_queries.json", "r") as f:
+                data = f.read()
+            queries = json.loads(data)
+
+        except Exception as e:
+            self.logger.warning("Failed to load config_emails.json or config_queries.json")
+            self.logger.error(str(e))
+            exit(0)
+
+
+
+    def email(self, config, ads):
+
+        if len(ads) == 0:
+            self.logger.info("Skipping emails (0 interesting ads)")
+            return False
+
+        try:
+            email_notifier = emails[config['notifier']]
+            email_notify = emails[config['notify']]
+        except:
+            self.logger.info("Email data missing")
+            return False
+
+        msg = "\r\n".join([
+          "From: {0}".format(email_notifier['email_address']),
+          "To: {0}".format(email_notify['email_address']),
+          "Subject: Ads report",
+          "",
+          str(ads) # format this to a better report
+          ])
+
+        try:
+            server = smtplib.SMTP(email_notifier['email_smtp'])
+            server.ehlo()
+            server.starttls()
+            server.login(email_notifier['email_address'],email_notifier['email_password'])
+            server.sendmail(email_notifier['email_address'], email_notify['email_address'], msg)
+            server.quit()
+        except Exception as e:
+            self.logger.info("Failed sending mail")
+            self.logger.info(str(e))
+            return False
+
+        return True
+
+
+
+    def run_at(self, dt):
+        pass
+
+    def run_once(self):
+        pass
+
+    def run_each(self):
+        pass
+
+
+adseeker = Adseeker()
+
+exit(0)
+
 
 
 ret = Milanuncios().query(
--- test.py
+++ test.py
@@ -1,6 +1,66 @@
 # -*- encoding: utf-8 -*-
 
 
+from datetime import timedelta, datetime
+from threading import Timer
+from time import sleep
+
+def hello_world():
+    print("it works")
+    #...
+
+
+last_time = datetime.now()
+
+while True:
+	if (datetime.now() - last_time).total_seconds() >= 5:
+		last_time = datetime.now()
+
+		hello_world()
+
+
+
+
+
+
+
+
+exit(0)
+
+
+
+
+import smtplib
+
+fromaddr = 'kener18@gmail.com'
+toaddrs  = 'kener18@gmail.com'
+
+username = 'kener18@gmail.com'
+password = 'dpn6j12m16-e2'
+
+msg = "\r\n".join([
+  "From: {0}".format(fromaddr),
+  "To: {0}".format(toaddrs),
+  "Subject: Just a message",
+  "",
+  "Why, oh why"
+  ])
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
+
+
+
+
+
+
+
+exit(0)
+
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
